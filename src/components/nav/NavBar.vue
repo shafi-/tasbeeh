d01d1bd4454a121c707fe926272df60448f359ef
shafi- <shafi.cse.buet@gmail.com>
2023-01-04 02:37:00 +0600

    Back button with navbar

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -2,20 +2,28 @@
   <!-- Create a container element for the navbar -->
   <nav class="p-3" :class="menuOpen ? 'bg-inherit' : 'bg-gray-800'">
     <!-- Create a toggle button for the navbar menu on small screens -->
-    <button
-      class="md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
-      :class="{ hidden: menuOpen }"
-      @click="toggleMenu"
-    >
-      <svg
-        class="fill-current h-3 w-3"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
+    <div :class="{ hidden: menuOpen }" class="flex justify-between md:hidden">
+      <button
+        class="border border-white flex items-center px-3 py-2 rounded text-white"
+        @click="toggleMenu"
       >
-        <title>Menu</title>
-        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-      </svg>
-    </button>
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+      <button
+        class="border-white flex items-center px-3 py-2 rounded text-white arrow-left"
+        @click="goBack"
+      >
+        back
+        <!-- <icon title="back" /> -->
+      </button>
+    </div>
     <!-- Create the navbar menu -->
     <transition :duration="0.4" mode="out-in">
       <div
@@ -89,6 +97,9 @@ export default defineComponent({
   methods: {
     toggleMenu() {
       this.$emit("menu-state");
+    },
+    goBack() {
+      this.$router.back();
     },
   },
 });
